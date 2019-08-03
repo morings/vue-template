9597b401a94a3f5266aee5a38621ad9e79682b0e
qcx <1620201780@qq.com>
2019-08-03 20:52:45 +0800

    扩展

--- template/src/tool.js
+++ template/src/tool.js
@@ -1,44 +1,60 @@
+//date对象方法
+Date.prototype.Format = function (fmt) { //author: meizz 
+  var o = {
+    "M+": this.getMonth() + 1, //月份 
+    "d+": this.getDate(), //日 
+    "h+": this.getHours(), //小时 
+    "m+": this.getMinutes(), //分 
+    "s+": this.getSeconds(), //秒 
+    "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+    "S": this.getMilliseconds() //毫秒 
+  };
+  if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+  for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+  return fmt;
+}
 //深拷贝函数
-export const deepClone = function(data){
+export const deepClone = function (data) {
   var type = getType(data);
   var obj;
-  if(type === 'array'){
-      obj = [];
-  } else if(type === 'object'){
-      obj = {};
+  if (type === 'array') {
+    obj = [];
+  } else if (type === 'object') {
+    obj = {};
   } else {
-      //不再具有下一层次
-      return data;
+    //不再具有下一层次
+    return data;
   }
-  if(type === 'array'){
-      for(var i = 0, len = data.length; i < len; i++){
-          obj.push(deepClone(data[i]));
-      }
-  } else if(type === 'object'){
-      for(var key in data){
-          obj[key] = deepClone(data[key]);
-      }
+  if (type === 'array') {
+    for (var i = 0, len = data.length; i < len; i++) {
+      obj.push(deepClone(data[i]));
+    }
+  } else if (type === 'object') {
+    for (var key in data) {
+      obj[key] = deepClone(data[key]);
+    }
   }
   return obj;
 }
 
-function getType(obj){
+function getType(obj) {
   //tostring会返回对应不同的标签的构造函数
   var toString = Object.prototype.toString;
   var map = {
-     '[object Boolean]'  : 'boolean', 
-     '[object Number]'   : 'number', 
-     '[object String]'   : 'string', 
-     '[object Function]' : 'function', 
-     '[object Array]'    : 'array', 
-     '[object Date]'     : 'date', 
-     '[object RegExp]'   : 'regExp', 
-     '[object Undefined]': 'undefined',
-     '[object Null]'     : 'null', 
-     '[object Object]'   : 'object'
- };
- if(obj instanceof Element) {
-      return 'element';
- }
- return map[toString.call(obj)];
+    '[object Boolean]': 'boolean',
+    '[object Number]': 'number',
+    '[object String]': 'string',
+    '[object Function]': 'function',
+    '[object Array]': 'array',
+    '[object Date]': 'date',
+    '[object RegExp]': 'regExp',
+    '[object Undefined]': 'undefined',
+    '[object Null]': 'null',
+    '[object Object]': 'object'
+  };
+  if (obj instanceof Element) {
+    return 'element';
+  }
+  return map[toString.call(obj)];
 }
