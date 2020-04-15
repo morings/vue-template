f2bc7a478ae5b782ccd9d620b7512618537223f7
qcx <1620201780@qq.com>
2020-04-15 17:49:57 +0800

    vue入口文件

--- template/src/main.js
+++ template/src/main.js
@@ -2,42 +2,41 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
-import 'element-ui/lib/theme-chalk/index.css'
+//引入初始化css
+import 'element-ui/lib/theme-chalk/index.css'//引入elmentui样式
 import './assets/styles/index.css'
 import Vue from 'vue'
-import Navigation from 'vue-navigation'
+import Navigation from 'vue-navigation'//前进刷新后退不刷新组件
 import ECharts from 'vue-echarts'
-import 'echarts'
-import 'babel-polyfill'
+import 'babel-polyfill'//es6打包转es5
 import ElementUI from 'element-ui'
 import 'viewerjs/dist/viewer.css'
-import Viewer from 'v-viewer'//引入图片预览插件
-import VueClipboard from 'vue-clipboard2'
-import animate from 'animate.css' //引入动画
+import Viewer from 'v-viewer'
+//引入图片预览插件
+import animate from 'animate.css' //引入动画animate.css
 import App from './App'
 import router from './router'
 import $http from "./Api/api"
 import {deepClone} from "./tool"
 import bus from "./Bus"
-import $cfm from "./confim"
-import { Message} from 'element-ui';
-import{setCookie_,getCookie,delCookie} from "./cookie"
+import {$cfm,$prt} from "./confim"
+import{getCookie,delCookie} from "./cookie"
 import './directive'
 import './globalMixin'
 Vue.use(Navigation, {router})
-ElementUI.TableColumn.props.showOverflowTooltip = {type:ElementUI.TableColumn.props.showOverflowTooltip,default:true};
-ElementUI.Select.props.filterable = {type:ElementUI.Select.props.filterable,default:true};
+ElementUI.TableColumn.props.showOverflowTooltip = {type:ElementUI.TableColumn.props.showOverflowTooltip,default:true};//设置elment的table文字超出显示省略号
+ElementUI.Select.props.filterable = {type:ElementUI.Select.props.filterable,default:true};//设置elment的select默认带有filter功能
 console.log(ElementUI)
 Vue.use(ElementUI,{ size: 'small'})
-Vue.use(VueClipboard);
 Vue.use(Viewer);
 Vue.use(animate)
-Vue.config.productionTip = false
+Vue.config.productionTip = false//阻止启动生产消息，常用作指令。
 Vue.prototype.$http = $http;
 Vue.prototype.$cfm = $cfm;
+Vue.prototype.$prt = $cfm;
 Vue.prototype.$deepClone = deepClone;
 Vue.prototype.$bus = bus;
-Vue.prototype.$setCookie = setCookie_;
+Vue.prototype.$setCookie = setCookie;
 Vue.prototype.$getCookie = getCookie;
 Vue.prototype.$delCookie = delCookie;
 Vue.component('v-chart', ECharts)
