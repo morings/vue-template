dc3b297613c18c0caa7b4437bddebccb93c56dfb
qcx <1620201780@qq.com>
2020-04-15 17:50:55 +0800

    单图片上传组件

--- template/src/components/common/AvatarUpload.vue
+++ template/src/components/common/AvatarUpload.vue
@@ -1,80 +1,105 @@
+//单图片上传组件
 <template>
-  <el-upload
-    v-if="!disabled"
-    class="avatar-uploader"
-    action=""
-    accept="'image/*'"           
-    :auto-upload="false"
-    :show-file-list="false"
-    :on-change="change">
-    <img v-if="imageUrl" :src="imageUrl" class="avatar">
-    <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-  </el-upload>
-  <img v-else :src="imageUrl" class="avatar" v-viewer>
+  <div class="Avatar-Upload">
+    <template v-if="url || disabled">
+      <img :src="url" alt="" class="avatar-uploader-img" ref="img" v-viewer>
+      <div class="hover" v-if='!disabled'>
+        <el-button type="primary" @click="selectFile">重传</el-button><el-button type="danger" @click="preview">预览</el-button>
+      </div>
+    </template>
+    <template v-else>
+      <i class="el-icon-plus avatar-uploader-icon" @click="selectFile"></i>
+    </template>
+    <input type="file" style="display:none" ref="input" accept='image/*' @change='changed'>
+  </div>
 </template>
 <script>
 export default {
   props:{
-    value:{
-
-    },
     disabled:{
-      default:false
+      default:false,
+      type:Boolean
+    },
+    value:{
+      
     }
   },
   data(){
-    return{
-      imageUrl:'',
+    return {
+      url:"",
+      file:null,
+      view:null
     }
-  },
-  mounted(){
-    this.getImage()
   },
   watch:{
     value(){
-      this.getImage()
+      if(this.value instanceof File){
+        this.url = URL.createObjectURL(this.value);
+      }else{
+        this.url = this.value
+      }
     }
   },
-  methods:{
-    //添加图片
-    change(file){
-      this.$emit('input',file.raw)
-    },
-    getImage(){
-      if(typeof(this.value)=='object'){
-        //是文件
-        this.imageUrl = URL.createObjectURL(this.value);
-      }else if(typeof(this.value)=='string'){
-        //是链接
-        this.imageUrl = this.value;
+  mounted(){
+    if(this.value instanceof File){
+        this.url = URL.createObjectURL(this.value);
+      }else{
+        this.url = this.value
       }
+  },
+  methods:{
+    selectFile(){
+      this.$refs.input.click();
+    },
+    changed(e){
+      this.file = this.$refs.input.files[0];
+      this.url = URL.createObjectURL(this.file);
+      this.$emit('input',this.file)
+    },
+    preview(){
+      this.$refs.img.click()
     }
   }
 }
 </script>
-<style lang="scss">
-  .avatar-uploader .el-upload {
-    border: 1px dashed #d9d9d9;
-    border-radius: 6px;
-    cursor: pointer;
-    position: relative;
-    overflow: hidden;
+<style lang="scss" scoped>
+.Avatar-Upload{
+  display: inline-block;
+  text-align: center;
+  border: 1px dashed #d9d9d9;
+  background-color: #fbfdff;
+  position: relative;
+  &:hover{
+    border-color: #409eff;
+    .hover{
+      opacity: 1;
+    }   
   }
-  .avatar-uploader .el-upload:hover {
-    border-color: #409EFF;
+  .hover{
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+    position: absolute;
+    top:0;
+    left: 0;
+    background-color: rgba(0,0,0,.5);
+    opacity: 0;
+    transition: opacity .3s;
   }
-  .avatar-uploader-icon {
+  .avatar-uploader-icon{
     font-size: 28px;
     color: #8c939d;
-    width: 148px;
-    height: 148px;
-    line-height: 148px;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
     text-align: center;
+    cursor: pointer;
   }
-  .avatar {
-    width: 148px;
-    height: 148px;
-    display: block;
+  .avatar-uploader-img{
+    width: 178px;
+    height: 178px;
+    cursor: pointer;
   }
+}
 </style>
-
